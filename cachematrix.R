b0ab3906a67991d699242a765a9f9cf6e576b198
elojan <40872567+elojan@users.noreply.github.com>
2020-07-19 14:33:32 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## mi solucion versin 2
+## crea funciones set, get para los valores de matriz
+## setMI y getMI aplican la funcion solve que calcula la inversa de una matriz cuadrada
+MatrizInv <- function(x = matrix(rnorm(9),3,3)) {
+  mi <- NULL
+  set <- function(y) {
+    x <<- y
+    mi <<- NULL
+  }
+  get <- function() x
+  setMI <- function(solve) mi <<- solve
+  getMI <- function() mi
+  list(set = set, get = get,
+       setMI = setMI,
+       getMI = getMI)
 }
 
 
-## Write a short comment describing this function
+## cacheMI: si mi se encuentra en cache muestra resultado
+## sin calcular nuevamente.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheMI <- function(x, ...) {
+  mi <- x$getMI()
+  if(!is.null(mi)) {
+    message("Ya tenemos la inversa de la matriz")
+    return(mi)
+  }
+  data <- x$get()
+  mi <- solve(data, ...)
+  x$setMI(mi)
+  mi
 }
